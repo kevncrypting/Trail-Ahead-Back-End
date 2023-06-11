bad82c6d0e0b526fa623a3fc1956b0ca193247c0
Thanuseng Kevin Prachith <tkprachith@gmail.com>
2023-06-11 16:55:24 -0400

    Adds all necessary routes for hikes, and updates route info on README

--- routes/hikes.js
+++ routes/hikes.js
@@ -19,4 +19,79 @@ router.get("/:id", function (req, res, next) {
         .catch((err) => next(err));
 });
 
+// POST (create) a new hike
+router.post("/", function (req, res, next) {
+    const {
+        hikePlanner,
+        trailName,
+        trailThumbnail,
+        trailCover,
+        timeDate,
+        currentGroupSize,
+        maxGroupSize,
+        about,
+        expectations,
+    } = req.body;
+
+    new Hike({
+        hikePlanner,
+        trailName,
+        trailThumbnail,
+        trailCover,
+        timeDate,
+        currentGroupSize,
+        maxGroupSize,
+        about,
+        expectations,
+    })
+        .save()
+        .then((hike) => res.json(hike))
+        .catch((err) => next(err));
+});
+
+// PUT (update) a specific hike by its ID
+router.put("/:id", function (req, res, next) {
+    const { id } = req.params;
+    const {
+        hikePlanner,
+        trailName,
+        trailThumbnail,
+        trailCover,
+        timeDate,
+        currentGroupSize,
+        maxGroupSize,
+        about,
+        expectations,
+    } = req.body;
+
+    Hike.where({ id })
+        .fetch()
+        .then((hike) => {
+            return hike.save({
+                hikePlanner,
+                trailName,
+                trailThumbnail,
+                trailCover,
+                timeDate,
+                currentGroupSize,
+                maxGroupSize,
+                about,
+                expectations,
+            });
+        })
+        .then((updatedHike) => res.json(updatedHike))
+        .catch((err) => next(err));
+});
+
+// DELETE a specific hike by its ID
+router.delete("/:id", function (req, res, next) {
+    const { id } = req.params;
+
+    Hike.where({ id })
+        .destroy()
+        .then((hike) => res.json(hike))
+        .catch((err) => next(err));
+});
+
+
 module.exports = router;
