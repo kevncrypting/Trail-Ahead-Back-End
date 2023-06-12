8d17c66e0a8650e619a8ee37a815807de07c6f25
Thanuseng Kevin Prachith <tkprachith@gmail.com>
2023-06-12 09:51:37 -0400

    Attempts different ways to create user

--- routes/auth.js
+++ routes/auth.js
@@ -5,22 +5,62 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { User } = require("../models/User");
 
-router.post("/register", async (req, res) => {
+router.post("/register", async (req, res, next) => {
     // deconstructs the username and password values from the request body
-    const { username, password } = req.body;
+    const { email, password, firstName, lastName } = req.body;
 
-    if (!username || !password) {
+    if (!email || !password) {
         // conditional block that checks to make sure the username and password values are not null/undefined - if one is, then returns the 400 status code and the message below as a json object
         return res
             .status(400)
             .json({ message: "Username and password are both required." });
     }
 
+    if (!firstName || !lastName) {
+        // conditional block that checks to make sure the username and password values are not null/undefined - if one is, then returns the 400 status code and the message below as a json object
+        return res.status(400).json({
+            message:
+                "Please enter a first and last name to identify yourself to the community.",
+        });
+    }
+
     try {
         // using bcrypt, creates a hashedPassword out of the password value that is the result of 10 salt rounds
-        const hashedPassword = bcrypt.hashSync(password, 10); 
-        await User.create({ username, password: hashedPassword });
-        res.sendStatus(201);
+        const hashedPassword = bcrypt.hashSync(password, 10);
+
+        // await User.create({
+        //     email,
+        //     password: hashedPassword,
+        //     firstName,
+        //     lastName,
+        // })
+        // .save()
+        // .then((user) => res.json(user).status(201))
+        // .catch((err) => next(err));
+
+        console.log("test try block");
+
+        // const user = await User.forge({
+        //     email,
+        //     password: hashedPassword,
+        //     firstName,
+        //     lastName,
+        // });
+
+        // console.log(user);
+
+        // user.save().then((user) => {
+        //     res.sendStatus(201).json(user);
+        // });
+
+        // await User.create({
+        //     username,
+        //     password: hashedPassword,
+        //     firstName,
+        //     lastName,
+        // });
+
+        // res.sendStatus(201);
     } catch (err) {
         // if an error is present, the error is sent back with the 500 status code
         res.status(500).send(err);
@@ -30,7 +70,7 @@ router.post("/register", async (req, res) => {
 router.post("/login", async (req, res, next) => {
     try {
         // looks for a User in the database where the username matches the username in the body of the request, then fetch that User
-        const user = await User.where({ username: req.body.username }).fetch(); 
+        const user = await User.where({ username: req.body.username }).fetch();
 
         if (!user) {
             // if user is null/undefined, return the 401 status code and following error message
@@ -43,7 +83,7 @@ router.post("/login", async (req, res, next) => {
         const isPasswordValid = await bcrypt.compare(
             req.body.password,
             user.password
-        ); 
+        );
 
         if (!isPasswordValid) {
             // if bcrypt cannot match the password, the variable isPasswordValid is false and this expression evaluates to be true - then returns the 401 status code and the following error message
