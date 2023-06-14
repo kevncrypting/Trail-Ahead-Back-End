67702e566ec126d6b911d2fd46fd9a083632f17c
Thanuseng Kevin Prachith <tkprachith@gmail.com>
2023-06-14 11:31:20 -0400

    Adds .env variables to wire up production database

--- knexfile.js
+++ knexfile.js
@@ -42,13 +42,16 @@ module.exports = {
 
     production: {
         client: "postgresql",
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
         connection: process.env.DATABASE_URL,
         pool: {
             min: 2,
             max: 10,
         },
         migrations: {
-            tableName: "knex_migrations",
+            directory: './migrations',
         },
     },
 };
