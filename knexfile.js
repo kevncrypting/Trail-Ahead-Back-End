75f54f6c63028f74134d2be1f4360dfe3520574b
Thanuseng Kevin Prachith <tkprachith@gmail.com>
2023-06-14 11:52:14 -0400

    Adds changes to knexfile

--- knexfile.js
+++ knexfile.js
@@ -41,10 +41,7 @@ module.exports = {
     },
 
     production: {
-        client: "pg",
-        username: process.env.DATABASE_USER,
-        password: process.env.DATABASE_PASSWORD,
-        database: process.env.DATABASE_NAME,
+        client: "postgresql",
         connection: process.env.DATABASE_URL,
         pool: {
             min: 2,
